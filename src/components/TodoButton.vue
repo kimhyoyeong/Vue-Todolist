d8c812626b41a2bcb048c285a37af06fa16911d4
김유빈 <yb.kim@stickint.kr>
2021-04-15 16:14:53 +0900

    TodoInput 작업중

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TodoButton",
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #fff;
+}
+</style>
